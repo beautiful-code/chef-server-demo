cae09f71f609378267ed7f00ce33c6d6764ae56e
Ravi Bhim <ravi.bhim@yahoo.com>
2013-10-09 18:30:33 +0530

    setting hostname and downloaded some cookbooks

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,5 +4,6 @@ node_name                'admin'
 client_key               '/Users/ravibhim/git/chef-server-demo/.chef/admin.pem'
 validation_client_name   'chef-validator'
 validation_key           '/Users/ravibhim/git/chef-server-demo/.chef/validation.pem'
-chef_server_url          'https://10.33.33.33'
+chef_server_url          'https://chef.ravi.com'
 syntax_check_cache_path  '/Users/ravibhim/git/chef-server-demo/.chef/syntax_check_cache'
+cookbook_path            [ '/Users/ravibhim/git/chef-server-demo/cookbooks' ]
